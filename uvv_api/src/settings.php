19ba7e118ebb78649f59198dc829a3b0a9a844f9
Phantase <phantase@free.fr>
2018-02-11 22:27:47 +0100

    Add settings for DB

--- uvv_api/src/settings.php
+++ uvv_api/src/settings.php
@@ -15,5 +15,14 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database settings
+        'db' => [
+            'host' => 'db',
+            'user' => 'unionvovinamwww',
+            'pass' => 'unionvovinamwww',
+            'dbname' => 'unionvovinamwww',
+            'charset' => 'utf8',
+        ],
     ],
 ];
