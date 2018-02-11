26e815377a451107aee715357a1b12ca0d8775de
Phantase <phantase@free.fr>
2018-02-11 23:01:37 +0100

    Routes for Adherents, get All and count

--- uvv_api/src/routes.php
+++ uvv_api/src/routes.php
@@ -3,12 +3,26 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+use Objects\AdherentMapper;
+
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/adherents/count', function (Request $request, Response $response, array $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    $this->logger->info("UVV API /adherents/count");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $adherent_mapper = new AdherentMapper($this->db);
+    $count = $adherent_mapper->count();
+
+    return $response->withJson(array('count' => $count), 201);
+});
+
+$app->get('/adherents', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("UVV API /adherents");
+
+    $adherent_mapper = new AdherentMapper($this->db);
+    $adherents = $adherent_mapper->getAdherents();
+
+    return $response->withJson($adherents, 201);
 });
